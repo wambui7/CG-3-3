1bcce5c5b4fad92d6b483ec105d7bf390c5fe916
wambui7 <shiko.sheila@gmail.com>
2018-07-26 18:34:33 -0500

    Add project files.

---
+++ statements/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
