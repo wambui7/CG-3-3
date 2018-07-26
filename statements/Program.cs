1202670cddf55fcef53eea91c60df7442b6fbdd7
wambui7 <shiko.sheila@gmail.com>
2018-07-26 18:59:13 -0500

    changes

--- statements/Program.cs
+++ statements/Program.cs
@@ -6,7 +6,34 @@ namespace statements
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // statements are complete thoughts whereby they are made up of one or expression it consist of a single line and ends with a semi colon 
+            //example 
+
+            string name = "cat";
+
+            //expression is made up of one or more operator where theres combination of operands,objects and variables
+            //example
+
+            string name = A + B + "";
+
+            // Operator its similar to verb and acts on operand ,and uses symbol that changes value 
+
+            //example * + =
+
+            int x = 5;
+            int y = 9;
+            int z = x + y;
+
+            //operands they are similar to noun they are the subject of the code and they  always define the value its normally the value changed by the operator
+            //example
+
+            int z = x + y;
+            
+            // z,x,y is the operand while + is the operator
+
+
+
+
         }
     }
 }
